60724a615e0af7ff2c880bc7e190bc08572631fc
Matan Moshe <115933834+Matanmoshes@users.noreply.github.com>
2024-08-10 22:57:41 +0300

    changes to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 # VPC Configuration
@@ -7,11 +7,38 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route Table for Public Subnets
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Public Subnets
 resource "aws_subnet" "app_subnet" {
   count             = 2
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+# Associate Subnets with the Route Table
+resource "aws_route_table_association" "public_rt_assoc" {
+  count          = 2
+  subnet_id      = element(aws_subnet.app_subnet[*].id, count.index)
+  route_table_id = aws_route_table.public_rt.id
 }
 
 data "aws_availability_zones" "available" {}
@@ -69,6 +96,7 @@ resource "aws_lb_target_group" "app_tg" {
   port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type = "ip"
 
   health_check {
     path                = "/"
