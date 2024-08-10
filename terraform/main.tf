bec22930e950d6febd7440504bb6697b8d99bcab
Matan Moshe <115933834+Matanmoshes@users.noreply.github.com>
2024-08-10 22:53:14 +0300

    add comment output eip

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,6 +117,6 @@ resource "aws_eip" "weather_app_eip" {
   vpc = true
 }
 
-output "elastic_ip" {
-  value = aws_eip.weather_app_eip.public_ip
-}
+#output "elastic_ip" {
+#  value = aws_eip.weather_app_eip.public_ip
+#}
