94247ee550275581698aeab5b5011ecf83275721
Matan Moshe <115933834+Matanmoshes@users.noreply.github.com>
2024-08-12 22:52:37 +0300

    SG PORT 80

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -4,6 +4,12 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
