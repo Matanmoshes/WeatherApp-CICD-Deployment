7fe09454d84f8308e5a22bcebd5d1bd88e7a90e7
Matan Moshe <115933834+Matanmoshes@users.noreply.github.com>
2024-08-14 20:45:07 +0300

    new workflow 2

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     weather_data = None
@@ -22,14 +23,15 @@ def index():
         }
         response = requests.get(BASE_URL, params=params)
         weather_data = response.json()
-        
+
         if weather_data and weather_data.get('timezone'):
             utc_time = datetime.utcnow()
             timezone_offset = weather_data['timezone']
             local_time = utc_time + timedelta(seconds=timezone_offset)
             local_time = local_time.strftime('%Y-%m-%d %H:%M:%S')
-    
+
     return render_template('index.html', weather_data=weather_data, local_time=local_time)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
