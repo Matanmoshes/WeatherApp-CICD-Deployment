25c3bc9175ae6207196930f1ae24b6c1166810c9
Matan Moshe <115933834+Matanmoshes@users.noreply.github.com>
2024-08-31 12:14:32 +0300

    syntx fix ver2

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -10,6 +10,10 @@ resource "aws_security_group" "alb_sg" {
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    security_groups = []
+    self = false
     } 
   ]
   # Allow all outbound traffic
@@ -19,6 +23,10 @@ resource "aws_security_group" "alb_sg" {
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    security_groups = []
+    self = false
     }
   ]
   tags = {
@@ -38,6 +46,10 @@ resource "aws_security_group" "ecs_sg" {
     to_port     = 5000
     protocol    = "tcp"
     security_groups = [aws_security_group.alb_sg.id]  # Allow traffic only from ALB security group
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    security_groups = []
+    self = false
     }
   ]
 
@@ -48,6 +60,10 @@ resource "aws_security_group" "ecs_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    security_groups = []
+    self = false
     }
   ]
 
