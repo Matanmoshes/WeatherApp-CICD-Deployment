fe26b94df13d0f163c830825b8ba10e0bc2d7b83
Matan Moshe <115933834+Matanmoshes@users.noreply.github.com>
2024-08-31 12:32:23 +0300

    new version

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -6,27 +6,26 @@ resource "aws_security_group" "alb_sg" {
   # Allow inbound HTTP traffic on port 80 from anywhere
   ingress = [
     {
+    description = "Allow HTTP traffic on port 80 from anywhere"
     from_port = 80
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
     prefix_list_ids = []
-    security_groups = []
     self = false
     } 
   ]
   # Allow all outbound traffic
   egress = [
     {
+    description = "Allow all outbound traffic"
     from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
     prefix_list_ids = []
-    security_groups = []
-    self = false
     }
   ]
   tags = {
@@ -42,28 +41,26 @@ resource "aws_security_group" "ecs_sg" {
   # Allow inbound traffic from the ALB on port 5000
   ingress = [
     {
+    description = "Allow inbound traffic from the ALB on port 5000"
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
     security_groups = [aws_security_group.alb_sg.id]  # Allow traffic only from ALB security group
     ipv6_cidr_blocks = []
     prefix_list_ids = []
-    security_groups = []
-    self = false
     }
   ]
 
   # Allow all outbound traffic
   egress = [ 
     {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
     prefix_list_ids = []
-    security_groups = []
-    self = false
     }
   ]
 
